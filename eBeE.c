512932dec21294a668dc0a585f39898e5c10b257
Ai Xin <scicomath@gmail.com>
2016-01-25 15:14:00 +0800

    跑循环

--- eBeE.c
+++ eBeE.c
@@ -12,10 +12,10 @@ int main(void)
   ag.epsabs = 1e-6;
   ag.flags = 0 | 8; // 0 | 8
   ag.seed = 0;
-  ag.smineval = 1.5e5;
-  ag.smaxeval = 1e6;
-  ag.pmineval = 2.5e5;
-  ag.pmaxeval = 1e7;
+  ag.smineval = 1.5e4; // 1.5e5
+  ag.smaxeval = 4e4;   // 1e6
+  ag.pmineval = 2e4; // 2.5e5
+  ag.pmaxeval = 5e4;   // 1e7
 
   ag.nstart = 1000;
   ag.nincrease = 500;
@@ -25,17 +25,29 @@ int main(void)
   ag.statefile = NULL;
   ag.spin = NULL;
 
-  x = 1.0;
-  y = 1.0;
+  x = 4.0;
+  y = 2.0;
   tau = 0.01;
   R = 7.0;
-  b = 4;
+  b = 10.0;
   Y0 = 5.36;
   a = 0.5;
   Z = 79.0;
-  eB(x, y, tau, R, b, Y0, a, Z, &ag, &eBy);
+  
+  int i, j;
+  for (i = 0; i <= 200; i++) { // i = 201; i <= 400; i++
+    for (j = 0; j <= 400; j++) {
+      x = -10. + (20.)*i/100.;
+      y = -10. + (20.)*j/100.;
+      eB(x, y, tau, R, b, Y0, a, Z, &ag, &eBy);
+      printf("%f %f %f\n",x, y, eBy);
+    }
+  }
 
-  printf("eBy = %5.3f\n", eBy);
+  
+  /* eB(x, y, tau, R, b, Y0, a, Z, &ag, &eBy); */
+
+  /* printf("eBy = %5.3f\n", eBy); */
   return 0;
   
 }
